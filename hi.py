37a536cfdc57f21ac563e53fde5676634007b0f9
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-02-11 15:26:09 +0530

    Add User interface

--- hi.py
+++ hi.py
@@ -10,7 +10,12 @@ import pywhatkit as kit
 import smtplib
 import sys
 import pyjokes
-
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+from jarvisGUI import Ui_JarvisGUI
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -21,25 +26,6 @@ def speak(audio):
     engine.say(audio)
     print(audio)
     engine.runAndWait()
-
-
-def takecommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("listening......")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-
-    try:
-        print("Recognizing.....")
-        query = r.recognize_google(audio)
-        print(f"user said: {query}")
-
-    except Exception as e:
-        speak("Say that again please.....")
-        return "none"
-    return query
-
 
 def wish():
     hour = int(datetime.datetime.now().hour)
@@ -60,115 +46,182 @@ def sendEmail(to, content):
     server.sendmail('your mail id', to, content)
     server.close()
 
+class MainThread(QThread):
 
-if __name__ == "__main__":
+    def __init__(self):
+        super(MainThread, self).__init__()
+    
+    def run(self):
+        self.taskExecution()
+        
+    def takecommand(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("listening......")
+            r.pause_threshold = 1
+            audio = r.listen(source)
+
+        try:
+            print("Recognizing.....")
+            query = r.recognize_google(audio)
+            print(f"user said: {query}")
+
+        except Exception as e:
+            speak("Say that again please.....")
+            return "none"
+        return query
+    
+    def taskExecution(self):
     # speak("hii hello")
-    wish()
-    while True:
-        query = takecommand().lower()
+        wish()
+        while True:
+            self.query = self.takecommand().lower()
 
-        if "open chrome" in query:
-            cr = "C:\\Program Files\\Google\Chrome\\Application\\chrome.exe"
-            os.startfile(cr)
+            if "open chrome" in self.query:
+                cr = "C:\\Program Files\\Google\Chrome\\Application\\chrome.exe"
+                os.startfile(cr)
 
-        elif "open command prompt" in query:
-            os.system("start cmd")
+            elif "open command prompt" in self.query:
+                os.system("start cmd")
 
-        elif "open camera" in query:
-            cap = cv2.VideoCapture(0)
-            while True:
-                ret, img = cap.read()
-                cv2.imshow('webcam', img)
-                k = cv2.waitKey(50)
-                if k == 27:
-                    break;
-            cap.release()
-            cv2.destroyAllWindows()
-        elif "play music" in query:
-            music_dir = "E:\\songs"
-            songs = os.listdir(music_dir)
-            for song in songs:
-                if song.endswith('.mp3'):
-                    os.startfile(os.path.join(music_dir, song))
-
-        elif "ip address" in query:
-            ip = get('https://api.ipify.org').text
-            speak(f"your ip address is {ip}")
-
-        elif "wikipedia" in query:
-            speak("searching wikipedia...")
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("according to wikipedia...")
-            speak(results)
-            print(results)
-
-        elif "news" in query:
-            from NewsRead import latestNews
-            latestNews()
-
-        elif "open youtube" in query:
-            speak("opening youtube")
-            webbrowser.open("www.youtube.com")
-
-        elif "open facebook" in query:
-            speak("opening facebook")
-            webbrowser.open("www.facebook.com")
-
-        elif "open linkedin" in query:
-            speak("opening linkedin")
-            webbrowser.open("www.linkedin.com")
-
-        elif "open google" in query:
-            speak("sir what do I search on google")
-            cm = takecommand().lower()
-            webbrowser.open(f"{cm}")
-
-        elif "send whatsapp message" in query:
-            kit.sendwhatmsg("+916204042821", "kyaa re bhikmangya", 20, 13)
-
-        elif "search songs on youtube" in query:
-            kit.playonyt("see you again")
-
-        elif "send email to vikas" in query:
-            try:
-                speak("what should i say?")
-                content = takecommand().lower()
-                to = "vikaskum1ar@gmail.com"
-                sendEmail(to, content)
-                speak("email has been sent")
-            except Exception as e:
-                print(e)
-                speak("sorry sir i am not able to send this email")
-
-        elif "no thanks" in query:
-            speak("thanks for using me sir, have a good day")
-            sys.exit()
-        elif "stop" in query:
-            speak("ok sir")
-            sys.exit()
-
-        elif "close chrome" in query:
-            speak("closing chrome sir")
-            os.system("taskkill /f /im chrome.exe")
-
-        elif "set alarm" in query:
-            nn = int(datetime.datetime.now().hour)
-            if nn == 00:
-                speak(f"alarm is set at {nn}")
+            elif "open camera" in self.query:
+                cap = cv2.VideoCapture(0)
+                while True:
+                    ret, img = cap.read()
+                    cv2.imshow('webcam', img)
+                    k = cv2.waitKey(50)
+                    if k == 27:
+                        break;
+                cap.release()
+                cv2.destroyAllWindows()
+            elif "play music" in self.query:
                 music_dir = "E:\\songs"
                 songs = os.listdir(music_dir)
-                os.startfile(os.path.join(music_dir, songs[1]))
+                for song in songs:
+                    if song.endswith('.mp3'):
+                        os.startfile(os.path.join(music_dir, song))
 
-        elif "tell me a joke" in query:
-            jk = pyjokes.get_joke()
-            speak(jk)
+            elif "ip address" in self.query:
+                ip = get('https://api.ipify.org').text
+                speak(f"your ip address is {ip}")
 
-        elif "shut down the system" in query:
-            os.system('shutdown /s /t 5')
-        elif "restart the system" in query:
-            os.system("shutdown /r /t 5")
-        elif "sleep the system" in query:
-            os.system("rund1132.exe powrprof.dll, SetSuspendState 0,1,0")
+            elif "wikipedia" in self.query:
+                speak("searching wikipedia...")
+                self.query = self.query.replace("wikipedia", "")
+                results = wikipedia.summary(self.query, sentences=2)
+                speak("according to wikipedia...")
+                speak(results)
+                print(results)
 
-        speak("sir, do you have any other work")
+            elif "news" in self.query:
+                from NewsRead import latestNews
+                latestNews()
+
+            elif "open youtube" in self.query:
+                speak("opening youtube")
+                webbrowser.open("www.youtube.com")
+
+            elif "open facebook" in self.query:
+                speak("opening facebook")
+                webbrowser.open("www.facebook.com")
+
+            elif "open linkedin" in self.query:
+                speak("opening linkedin")
+                webbrowser.open("www.linkedin.com")
+
+            elif "open google" in self.query:
+                speak("sir what do I search on google")
+                cm = self.takecommand().lower()
+                webbrowser.open(f"{cm}")
+
+            elif "send whatsapp message" in self.query:
+                kit.sendwhatmsg("+916204042821", "kyaa re bhikmangya", 20, 13)
+
+            elif "search songs on youtube" in self.query:
+                kit.playonyt("see you again")
+
+            elif "send email to vikas" in self.query:
+                try:
+                    speak("what should i say?")
+                    content = self.takecommand().lower()
+                    to = "vikaskum1ar@gmail.com"
+                    sendEmail(to, content)
+                    speak("email has been sent")
+                except Exception as e:
+                    print(e)
+                    speak("sorry sir i am not able to send this email")
+
+            elif "no thanks" in self.query:
+                speak("thanks for using me sir, have a good day")
+                sys.exit()
+            elif "stop" in self.query:
+                speak("ok sir")
+                sys.exit()
+
+            elif "close chrome" in self.query:
+                speak("closing chrome sir")
+                os.system("taskkill /f /im chrome.exe")
+
+            elif "set alarm" in self.query:
+                nn = int(datetime.datetime.now().hour)
+                if nn == 00:
+                    speak(f"alarm is set at {nn}")
+                    music_dir = "E:\\songs"
+                    songs = os.listdir(music_dir)
+                    os.startfile(os.path.join(music_dir, songs[1]))
+
+            elif "tell me a joke" in self.query:
+                jk = pyjokes.get_joke()
+                speak(jk)
+
+            elif "shut down the system" in self.query:
+                os.system('shutdown /s /t 5')
+            elif "restart the system" in self.query:
+                os.system("shutdown /r /t 5")
+            elif "sleep the system" in self.query:
+                os.system("rund1132.exe powrprof.dll, SetSuspendState 0,1,0")
+
+            speak("sir, do you have any other work")
+
+startExecution = MainThread()
+
+class UserInterface(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_JarvisGUI()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.startTask)
+        self.ui.pushButton_2.clicked.connect(self.close)
+
+    def startTask(self):
+        self.ui.movie = QtGui.QMovie("Gifs/MImN.gif")
+        self.ui.label.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        self.ui.movie = QtGui.QMovie("Gifs/7kmF.gif")
+        self.ui.label_2.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        self.ui.movie = QtGui.QMovie("Gifs/SUV4.gif")
+        self.ui.label_4.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        self.ui.movie = QtGui.QMovie("Gifs/Jarvis_Loading_Screen.gif")
+        self.ui.label_5.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        self.ui.movie = QtGui.QMovie("Gifs/7fi4")
+        self.ui.label_3.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        timer = QTimer(self)
+        timer.timeout.connect(self.showTime)
+        timer.start(1000)
+        startExecution.start()
+
+    def showTime(self):
+        current_time = QTime.currentTime()
+        current_date = QDate.currentDate()
+        time = current_time.toString("hh:mm:ss")
+        date = current_date.toString("dd/MM/yyyy")
+        self.ui.textBrowser.setText("\n\nDate: "+date+"\n\nTime: "+time)
+
+app = QApplication(sys.argv)
+jarvis = UserInterface()
+jarvis.show()
+exit(app.exec_())
